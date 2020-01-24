c44e33451c5e2db5cd26a1a02891727d41a5e3ea
Christian Chavez <christiancchavez10@hotmail.com>
2020-01-24 17:21:37 -0500

    finish of add dinamyc HTML to layout

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 
-const imagesCharacters = [
+const characters = [
     {name:'Kanye West', image:'./Images/kanye.png', category:'Entertainment'},
     {name:'Mark Suckenberg', image:'./Images/kanye..png', category:'Bussines'},
     {name:'Cristina Fernández de Kirchner', image:'./Images/cristina.png', category:'Politics'},
@@ -7,12 +7,12 @@ const imagesCharacters = [
 ]
 
 let mainCharacters = document.getElementById('mainCharacters');
-for(i=0; i < 3; i++){
-    const characters = imagesCharacters[i];
-    console.log(foursome);
+for(i=0; i <= 3; i++){
+    const character = characters[i];
 
     let character = document.createElement('div');
     character.classList.add('character');
+    character.style.background = url(`${character.image}`);
 
     let characterProfile = document.createElement('div');
     characterProfile.classList.add('character-profile');
@@ -27,10 +27,10 @@ for(i=0; i < 3; i++){
     characterProfileInfo.classList.add('character-profile__info');
 
     let characterProfileInfoName = document.createElement('h2');
-    characterProfileInfoName.innerHTML=`${characters.name}`;
+    characterProfileInfoName.innerHTML=`${character.name}`;
 
     let ProfileReview = document.createElement('span');
-    characterProfileInfo.innerHTML= `1 month ago in ${characters.category}`
+    characterProfileInfo.innerHTML= `1 month ago in ${character.category}`
 
     let characterReview = document.createElement('p');
     characterReview.classList.add('character__review');
@@ -39,8 +39,37 @@ for(i=0; i < 3; i++){
     let characterVotes = document.createElement('div');
     characterVotes.classList.add('character-votes');
 
-    let thumbsVote = document.createElement('div');
-    thumbsVote.classList.add('thumbs__thumb thumbs__thumb--up vote');
+    let thumbsVoteUp = document.createElement('div');
+    thumbsVoteUp.classList.add('thumbs__thumb thumbs__thumb--up vote');
+
+    let spanVotesUp = document.createElement('span');
+    spanVotesUp.classList.add('thumbs__logo');
+
+    let thumbsVoteDown = document.createElement('div');
+    thumbsVoteDown.classList.add('thumbs__thumb thumbs__thumb--down vote');
+
+    let spanVotesDown = document.createElement('span');
+    spanVotesDown.classList.add('thumbs__logo thumbs__logo--down');    
+
+    let characterVotesAdv = document.createElement('span');
+    characterVotesAdv.classList.add('character-votes__adv'); 
+    characterVotesAdv.innerHTML = 'Vote now';
+
+    let voting = document.createElement('div');
+    characterVotesAdv.classList.add('character-votes__adv'); 
+
+    let thumbsVotingUp = document.createElement('div');
+    thumbsVotingUp.classList.add('thumbs__thumb thumbs__thumb--up'); 
+
+    let spanVotingUp = document.createElement('span');
+    spanVotingUp.classList.add('thumbs__logo'); 
+
+    let thumbsVotingDown = document.createElement('div');
+    thumbsVotingDown.classList.add('thumbs__thumb thumbs__thumb--down'); 
+
+    let spanVotingDown = document.createElement('span');
+    spanVotingDown.classList.add('thumbs__logo thumbs__logo--down'); 
+
 
 
     mainCharacters.appendChild(character);
@@ -51,28 +80,15 @@ for(i=0; i < 3; i++){
     characterProfileInfo.appendChild(characterProfileInfoName);
     characterProfileInfo.appendChild(ProfileReview);
     character.appendChild(characterReview);
-
-
-
+    character.appendChild(characterVotes);
+    characterVotes.appendChild(thumbsVoteUp);
+    thumbsVoteUp.appendChild(spanVotesUp);
+    characterVotes.appendChild(thumbsVoteDown);
+    thumbsVoteDown.appendChild(spanVotesDown);
+    characterVotes.appendChild(characterVotesAdv);
+    character.appendChild(voting);
+    voting.appendChild(thumbsVotingUp);
+    thumbsVotingUp.appendChild(spanVotingUp);
+    voting.appendChild(thumbsVotingDown);
+    thumbsVotingDown.appendChild(spanVotingDown);
 }
-
-{/* <div class="character">
-        <div class="character-profile">
-            <div class="thumbs__thumb thumbs__thumb--up small "><span class="thumbs__logo" id="thumb"></span></div>
-            <div class="character-profile__info">
-                <h2>Kanye West</h2>
-                <span><b>1 month ago</b>in Entertainment</span>
-            </div>
-        </div>
-        <p class="character__review">Vestibulum diam ante, porttitor a odio eget, rhoncus neque. Aenean eu velit libero</p>
-        <div class="character-votes">
-            <div class="thumbs__thumb thumbs__thumb--up vote"><span class="thumbs__logo"></span></div>
-            <div class="thumbs__thumb thumbs__thumb--down vote"><span class="thumbs__logo thumbs__logo--down"></span></div>
-            <span class="character-votes__adv">Vote now</span>
-        </div>
-        <div class="voting">
-            <div class="thumbs__thumb thumbs__thumb--up"><span class="thumbs__logo"></span></div>
-            <div class="thumbs__thumb thumbs__thumb--down"><span class="thumbs__logo thumbs__logo--down"></span></div>
-        </div>
-    </div> 
-*/}
