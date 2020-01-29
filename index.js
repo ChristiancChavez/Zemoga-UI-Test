802a1e6f22a76c0f8312f9a7ab36b0b5a3c00626
Christian Chavez <christiancchavez10@hotmail.com>
2020-01-29 11:20:29 -0500

    fixed styles of characters.sass file

--- index.js
+++ index.js
@@ -6,78 +6,77 @@ const characters = [
     {name:'Malala Youzafzai', image:'./Images/malala.png', category:'Entertainment'},
 ]
 
+let character = document.createElement('div');
+let characterProfile = document.createElement('div');
+let thumbsThumb = document.createElement('div');
+let spanThumbs = document.createElement('span');
+let characterProfileInfo = document.createElement('div');
+let characterProfileInfoName = document.createElement('h2');
+let profileReview = document.createElement('span');
+let characterReview = document.createElement('p');
+let characterVotes = document.createElement('div');
+let thumbsVoteUp = document.createElement('div');
+let spanVotesUp = document.createElement('span');
+let thumbsVoteDown = document.createElement('div');
+let spanVotesDown = document.createElement('span');
+let characterVotesAdv = document.createElement('span');
+let voting = document.createElement('div');
+let thumbsVotingUp = document.createElement('div');
+let spanVotingUp = document.createElement('span');
+let thumbsVotingDown = document.createElement('div');
+let spanVotingDown = document.createElement('span');
+
+
+
+
+
+const getIdThumbVote = (e) => {
+    const borderWhite = 'solid 2px white'
+    let idThumb =  e.target.id;
+    if(idThumb === 'upVote'){
+        thumbsVoteUp.style.border = borderWhite
+    }
+    else {
+        thumbsVoteDown.style.border = borderWhite
+    }
+}
+
+
 let mainCharacters = document.getElementById('mainCharacters');
-for(i=0; i <= 3; i++){
-    const people = characters[i];
-    console.log(people)
-
-    let character = document.createElement('div');
+characters.map(people => {
     character.classList.add('character');
-    character.style.background = `url(${people.image})`;
-
-    let characterProfile = document.createElement('div');
+    character.style.background = `url(${people.image}) no-repeat center`;
+    character.style.backgroundSize = '140% 130%'
     characterProfile.classList.add('character-profile');
-
-    let thumbsThumb = document.createElement('div');
     thumbsThumb.classList.add('thumbs__thumb', 'thumbs__thumb--up', 'small');
-    
-    let spanThumbs = document.createElement('span');
     spanThumbs.classList.add('thumbs__logo');
-
-    let characterProfileInfo = document.createElement('div');
     characterProfileInfo.classList.add('character-profile__info');
-
-    let characterProfileInfoName = document.createElement('h2');
     characterProfileInfoName.innerHTML=`${people.name}`;
-
-    let profileReview = document.createElement('span');
     profileReview.innerHTML= `1 month ago in ${people.category}`
-
-    let characterReview = document.createElement('p');
     characterReview.classList.add('character__review');
     characterReview.innerHTML=`Vestibulum diam ante, porttitor a odio eget, rhoncus neque. Aenean eu velit libero`;
-
-    let characterVotes = document.createElement('div');
     characterVotes.classList.add('character-votes');
-
-    let thumbsVoteUp = document.createElement('div');
     thumbsVoteUp.classList.add('thumbs__thumb', 'thumbs__thumb--up', 'vote');
-
-    let spanVotesUp = document.createElement('span');
+    thumbsVoteUp.setAttribute("id", "upVote");
+    thumbsVoteUp.addEventListener("click", getIdThumbVote);
     spanVotesUp.classList.add('thumbs__logo');
-
-    let thumbsVoteDown = document.createElement('div');
     thumbsVoteDown.classList.add('thumbs__thumb', 'thumbs__thumb--down', 'vote');
-
-    let spanVotesDown = document.createElement('span');
+    thumbsVoteDown.setAttribute("id", "downVote");
+    thumbsVoteDown.addEventListener("click", getIdThumbVote);
     spanVotesDown.classList.add('thumbs__logo', 'thumbs__logo--down');    
-
-    let characterVotesAdv = document.createElement('span');
     characterVotesAdv.classList.add('character-votes__adv'); 
     characterVotesAdv.innerHTML = 'Vote now';
-
-    let voting = document.createElement('div');
     voting.classList.add('voting'); 
-
-    let thumbsVotingUp = document.createElement('div');
     thumbsVotingUp.classList.add('thumbs__thumb', 'thumbs__thumb--up'); 
-
-    let spanVotingUp = document.createElement('span');
     spanVotingUp.classList.add('thumbs__logo'); 
-
-    let thumbsVotingDown = document.createElement('div');
     thumbsVotingDown.classList.add('thumbs__thumb', 'thumbs__thumb--down'); 
-
-    let spanVotingDown = document.createElement('span');
     spanVotingDown.classList.add('thumbs__logo', 'thumbs__logo--down'); 
-
-
 
     mainCharacters.appendChild(character);
     character.appendChild(characterProfile);
     characterProfile.appendChild(thumbsThumb);
     thumbsThumb.appendChild(spanThumbs);
-    character.appendChild(characterProfileInfo);
+    characterProfile.appendChild(characterProfileInfo);
     characterProfileInfo.appendChild(characterProfileInfoName);
     characterProfileInfo.appendChild(profileReview);
     character.appendChild(characterReview);
@@ -92,4 +91,6 @@ for(i=0; i <= 3; i++){
     thumbsVotingUp.appendChild(spanVotingUp);
     voting.appendChild(thumbsVotingDown);
     thumbsVotingDown.appendChild(spanVotingDown);
-}
+});
+
+    
